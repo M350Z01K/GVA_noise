4a12e6c6032bad7df14a83e298871cdf53e061a6
mesozoik <61693941+M350Z01K@users.noreply.github.com>
2024-02-07 13:50:51 +0400

    cleaning

--- prelim_steps.R
+++ prelim_steps.R
@@ -1,12 +1,12 @@
-# library(sf)
-# library(raster)
-# library(readr)
-# 
-# 
-# 
-# can = st_read("data/swissBOUNDARIES3D_1_5_TLM_KANTONSGEBIET.shp")
-# statpop = readr::read_delim("data/ag-b-00.03-vz2022statpop/STATPOP2022.csv", delim = ";", escape_double = FALSE, trim_ws = TRUE)
-# road_noise = raster("data/STRASSENLAERM_Nacht/StrassenLaerm_Nacht_LV95.tif")
-# 
-# save(can, statpop, road_noise, file = "data.RData")
-# 
+library(sf)
+library(raster)
+library(readr)
+
+
+
+can = st_read("data/swissBOUNDARIES3D_1_5_TLM_KANTONSGEBIET.shp")
+statpop = readr::read_delim("data/ag-b-00.03-vz2022statpop/STATPOP2022.csv", delim = ";", escape_double = FALSE, trim_ws = TRUE)
+road_noise = raster("data/STRASSENLAERM_Nacht/StrassenLaerm_Nacht_LV95.tif")
+
+save(can, statpop, road_noise, file = "data.RData")
+
